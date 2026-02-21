947f53caf8ff9a29ecc925433367254a0a723fb9
mrhammad04x <jagralahammadimran00@gmail.com>
2026-02-21 12:09:45 +0530

    created folder structure for safety&transparency

---
+++ src/app/(routes)/safety&transparency/page.js
@@ -0,0 +1,17 @@
+import FAQ from "@/app/components/pricing/Faq";
+import FullPricing from "@/app/components/pricing/FullPricing";
+import NoCommissionModel from "@/app/components/pricing/NoCommissionModel";
+import PricingHero from "@/app/components/pricing/PricingHero";
+import TierCta from "@/app/components/pricing/TierCta";
+
+
+export default function SafetyAndTransparency() {
+    return (
+        <>
+        <FullPricing/>
+        <NoCommissionModel/>
+        <FAQ/>
+        <TierCta/>
+        </>
+    );
+}
